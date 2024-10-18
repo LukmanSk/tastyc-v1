a5bcd06be9f2a3043c102802d494a9c585dbc825
Lukman Sk <skd1746289@gmail.com>
2024-10-18 09:06:31 +0530

    F1: added fonts and assets

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,16 @@
-import localFont from "next/font/local";
+import { Italiana, Roboto } from "next/font/google";
 import "./globals.css";
+
+const italiana = Italiana({
+  subsets: ["latin"],
+  weight: ["400"],
+  variable: "--font-italiana",
+});
+const roboto = Roboto({
+  subsets: ["latin"],
+  weight: ["100", "300", "400", "500", "700", "900"],
+  variable: "--font-roboto",
+});
 
 export const metadata = {
   title: "Create Next App",
@@ -9,7 +20,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={`antialiased`}>{children}</body>
+      <body className={`${italiana.variable} ${roboto.variable} antialiased`}>
+        {children}
+      </body>
     </html>
   );
 }
